256c867fc0a349e56d7076f3ba9e9e3c43f0804f
Ivan Brilata <brilataivan86@gmail.com>
2024-09-08 20:13:05 +0800

    reponsiveness

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -16,7 +16,7 @@ const Banner = () => {
         }}
       >
         <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold mb-4">BE CREATIVE</h1>
-        <p className="text-lg md:text-xl mb-6">View IVAN'S Website to Learn More About Him.</p>
+        <p className="text-sm sm:text-base md:text-lg lg:text-xl mb-6">View IVAN'S Website to Learn More About Him.</p>
         <div>
           <button className="bg-transparent border-2 border-gray-500 text-white py-2 px-6 rounded-full font-bold hover:bg-gray-700 transition-colors duration-300 mx-2">
             <a className="mainbutton" href="#home" target="_blank">BROWSE MORE</a>
